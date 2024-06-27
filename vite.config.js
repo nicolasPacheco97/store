185889e873dc95cff91c46ac5c67f7db645a195a
nicolasPacheco97 <80474588+nicolasPacheco97@users.noreply.github.com>
2024-06-27 14:12:46 -0600

    [NPL]: 🚀Adding route name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/store",
   plugins: [
     react(),
     federation({
